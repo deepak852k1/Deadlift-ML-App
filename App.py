b513c65bcc02db681ca9f494cf16be18a6bb5b3e
deepak852k1 <deepak852k1@gmail.com>
2022-09-30 09:47:51 +0530

    import successful

--- App.py
+++ App.py
@@ -1,1 +1,10 @@
-print('Hello World')
+import tkinter as tk
+import customtkinter as ck
+import cv2
+import pandas as pd
+import numpy as np
+import pickle
+import mediapipe as mp
+import cv2
+from PIL import Image , ImageTk
+from landmarks import landmark
